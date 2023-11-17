4868b2cdffa658f4da78285f07597c6ed06e99fe
YuichiFujita <221065@st.yoshida-g.ac.jp>
2023-11-17 11:42:25 +0900

    エントリー時のカメラ位置変更

--- 00_ソースファイル/project000_LAMP/camera.cpp
+++ 00_ソースファイル/project000_LAMP/camera.cpp
@@ -37,11 +37,11 @@ namespace
 	namespace entry
 	{
 		const D3DXVECTOR3 INIT_VECU = D3DXVECTOR3(0.0f, 1.0f, 0.0f);		// 上方向ベクトルの初期値
-		const D3DXVECTOR3 INIT_POSV = D3DXVECTOR3(190.0f, 45.0f, -200.0f);	// 視点の初期値
-		const D3DXVECTOR3 INIT_POSR = D3DXVECTOR3(190.0f, 45.0f, 0.0f);		// 注視点の初期値
+		const D3DXVECTOR3 INIT_POSV = D3DXVECTOR3(360.0f, 100.0f, -200.0f);	// 視点の初期値
+		const D3DXVECTOR3 INIT_POSR = D3DXVECTOR3(360.0f, 100.0f, 0.0f);	// 注視点の初期値
 
-		const float VIEW_WIDTH	= SCREEN_WIDTH * 0.4f;	// 画面の横幅
-		const float VIEW_HEIGHT	= SCREEN_HEIGHT * 0.4f;	// 画面の縦幅
+		const float VIEW_WIDTH	= SCREEN_WIDTH * 0.75f;		// 画面の横幅
+		const float VIEW_HEIGHT	= SCREEN_HEIGHT * 0.75f;	// 画面の縦幅
 	}
 
 	// 回転カメラ情報
