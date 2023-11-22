bc52415a48e9af436cd3515631c5195db95d4de4
藤田勇一 <221065@st.yoshida-g.ac.jp>
2023-11-22 10:47:24 +0900

    spawnPoint.cpp,hを追加:プレイヤーのスポーン情報を管理する処理 object:LABELにSPAWNPOINTを追加

--- 00_ソースファイル/project000_LAMP/object.h
+++ 00_ソースファイル/project000_LAMP/object.h
@@ -59,6 +59,7 @@ public:
 		LABEL_UI,			// UI
 		LABEL_NUMBER,		// 数字
 		LABEL_PLAYER,		// プレイヤー
+		LABEL_SPAWNPOINT,	// 生成位置
 		LABEL_GROUND,		// 地盤
 		LABEL_BLOCK,		// ブロック
 		LABEL_OBSTACLE,		// 障害物
