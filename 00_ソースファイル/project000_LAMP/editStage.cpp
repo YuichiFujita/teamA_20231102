a5f08106ebe184f3b035e7321754d1a2bb4b77be
藤田勇一 <221065@st.yoshida-g.ac.jp>
2023-11-23 17:38:45 +0900

    editSpawnPoint.cpp,hを追加:生成位置の配置を管理するエディター たんのごめ～んね；； editStage:Stageの文字Groundになってるのキモイから修正、editSpawnPointの配置物とその生成追加 editStageManager:配置物追加に伴う表示文字列用の配列追加

--- 00_ソースファイル/project000_LAMP/editStage.cpp
+++ 00_ソースファイル/project000_LAMP/editStage.cpp
@@ -13,6 +13,7 @@
 #include "editGround.h"
 #include "editBlock.h"
 #include "editObstacle.h"
+#include "editSpawnPoint.h"
 
 //************************************************************
 //	親クラス [CEditStage] のメンバ関数
@@ -47,24 +48,28 @@ CEditStage *CEditStage::Create(CEditStageManager *pEdit, const EThing thing)
 #if _DEBUG
 
 	// ポインタを宣言
-	CEditStage *pEditGround = NULL;	// エディットステージ生成用
+	CEditStage *pEditStage = NULL;	// エディットステージ生成用
 
-	if (pEditGround == NULL)
+	if (pEditStage == NULL)
 	{ // 使用されていない場合
 
 		// メモリ確保
 		switch (thing)
 		{ // 配置物ごとの処理
 		case THING_GROUND:
-			pEditGround = new CEditGround;		// エディット地盤
+			pEditStage = new CEditGround;		// エディット地盤
 			break;
 
 		case THING_BLOCK:
-			pEditGround = new CEditBlock;		// エディットブロック
+			pEditStage = new CEditBlock;		// エディットブロック
 			break;
 
 		case THING_OBSTACLE:
-			pEditGround = new CEditObstacle;	// エディット障害物
+			pEditStage = new CEditObstacle;		// エディット障害物
+			break;
+
+		case THING_SPAWNPOINT:
+			pEditStage = new CEditSpawnPoint;	// エディット生成位置
 			break;
 
 		default:
@@ -74,26 +79,26 @@ CEditStage *CEditStage::Create(CEditStageManager *pEdit, const EThing thing)
 	}
 	else { assert(false); return NULL; }	// 使用中
 
-	if (pEditGround != NULL)
+	if (pEditStage != NULL)
 	{ // 使用されている場合
 
 		// エディットステージの情報を設定
-		pEditGround->m_pEdit = pEdit;
+		pEditStage->m_pEdit = pEdit;
 		
 		// エディットステージの初期化
-		if (FAILED(pEditGround->Init()))
+		if (FAILED(pEditStage->Init()))
 		{ // 初期化に失敗した場合
 
 			// メモリ開放
-			delete pEditGround;
-			pEditGround = NULL;
+			delete pEditStage;
+			pEditStage = NULL;
 
 			// 失敗を返す
 			return NULL;
 		}
 
 		// 確保したアドレスを返す
-		return pEditGround;
+		return pEditStage;
 	}
 	else { assert(false); return NULL; }	// 確保失敗
 
@@ -108,19 +113,19 @@ CEditStage *CEditStage::Create(CEditStageManager *pEdit, const EThing thing)
 //============================================================
 //	破棄処理
 //============================================================
-HRESULT CEditStage::Release(CEditStage *&prEditGround)
+HRESULT CEditStage::Release(CEditStage *&prEditStage)
 {
 #if _DEBUG
 
-	if (prEditGround != NULL)
+	if (prEditStage != NULL)
 	{ // 使用中の場合
 
 		// エディットステージの終了
-		prEditGround->Uninit();
+		prEditStage->Uninit();
 
 		// メモリ開放
-		delete prEditGround;
-		prEditGround = NULL;
+		delete prEditStage;
+		prEditStage = NULL;
 
 		// 成功を返す
 		return S_OK;
