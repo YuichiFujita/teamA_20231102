2acd693c93f0ac43d402eabd323f07ee79e68ccf
RikuNakamura <221050@st.yoshida-g.ac.jp>
2023-12-01 10:47:18 +0900

    消失バグの解決、フレイルの引き戻し調整

--- 00_ソースファイル/project000_LAMP/flail.cpp
+++ 00_ソースファイル/project000_LAMP/flail.cpp
@@ -257,13 +257,21 @@ void CFlail::UpdateChain(void)
 			if (player->GetCounterFlail() == flail::FLAIL_DROP && m_fChainRotMove > -0.01f && m_fChainRotMove < 0.01f)
 			{
 				D3DXVECTOR3 vecPtoF = VEC3_ZERO;
+				D3DXVECTOR3 vecPtoFTarget = VEC3_ZERO;
 				float lengthPtoF = 0.0f;
+				float lengthPtoFTarget = 0.0f;
+
+				vecPtoFTarget.x = player->GetMultiModel(CPlayer::MODEL_STICK)->GetPtrMtxWorld()->_41 - GetVec3Position().x;
+				vecPtoFTarget.y = player->GetMultiModel(CPlayer::MODEL_STICK)->GetPtrMtxWorld()->_42 - GetVec3Position().y;
+				vecPtoFTarget.z = player->GetMultiModel(CPlayer::MODEL_STICK)->GetPtrMtxWorld()->_43 - GetVec3Position().z;
+
 				vecPtoF.x = player->GetMultiModel(CPlayer::MODEL_STICK)->GetPtrMtxWorld()->_41 - GetVec3Position().x;
 				vecPtoF.z = player->GetMultiModel(CPlayer::MODEL_STICK)->GetPtrMtxWorld()->_43 - GetVec3Position().z;
 
-				m_fChainRot = atan2f(vecPtoF.x, vecPtoF.z) + D3DX_PI * 0.5f;
+				m_fChainRot = atan2f(vecPtoFTarget.x, vecPtoFTarget.z) + D3DX_PI * 0.5f;
 
 				lengthPtoF = D3DXVec3Length(&vecPtoF);
+				lengthPtoFTarget = D3DXVec3Length(&vecPtoFTarget);
 
 				if (lengthPtoF > flail::FLAIL_RADIUS * (flail::FLAIL_NUM - 1))
 				{
@@ -274,14 +282,23 @@ void CFlail::UpdateChain(void)
 					lengthPtoF = 0.0f;
 				}
 
-				m_fLengthTarget = lengthPtoF;
+				if (lengthPtoFTarget > flail::FLAIL_RADIUS * (flail::FLAIL_NUM - 1))
+				{
+					lengthPtoFTarget = flail::FLAIL_RADIUS * (flail::FLAIL_NUM - 1);
+				}
+				else if (lengthPtoFTarget < 0.0f)
+				{
+					lengthPtoFTarget = 0.0f;
+				}
 
-				if (m_fLengthTarget >= flail::FLAIL_RADIUS * (flail::FLAIL_NUM - 1))
+				m_fLengthTarget = lengthPtoFTarget;
+
+				if (lengthPtoFTarget >= flail::FLAIL_RADIUS * (flail::FLAIL_NUM - 1))
 				{
 					pos = GetVec3Position();
 
-					pos.x = player->GetMultiModel(CPlayer::MODEL_STICK)->GetPtrMtxWorld()->_41 + (sinf(m_fChainRot + D3DX_PI * 0.5f) * m_fLengthTarget);
-					pos.z = player->GetMultiModel(CPlayer::MODEL_STICK)->GetPtrMtxWorld()->_43 + (cosf(m_fChainRot + D3DX_PI * 0.5f) * m_fLengthTarget);
+					pos.x = player->GetMultiModel(CPlayer::MODEL_STICK)->GetPtrMtxWorld()->_41 + (sinf(m_fChainRot + D3DX_PI * 0.5f) * lengthPtoF);
+					pos.z = player->GetMultiModel(CPlayer::MODEL_STICK)->GetPtrMtxWorld()->_43 + (cosf(m_fChainRot + D3DX_PI * 0.5f) * lengthPtoF);
 
 					SetVec3Position(pos);
 				}
@@ -310,9 +327,9 @@ void CFlail::UpdateChain(void)
 
 			if (player->GetCounterFlail() <= flail::FLAIL_CHARGE && player->GetCounterFlail() > flail::FLAIL_DEF)
 			{
-				if ((m_chain[IDParent].multiModel->GetVec3Position().x >= flail::FLAIL_RADIUS && m_fLengthChain <= m_fLengthTarget) || nCntChain == 1)
+				if ((m_chain[IDParent].multiModel->GetVec3Position().x >= flail::FLAIL_RADIUS && m_fLengthChain < m_fLengthTarget) || nCntChain == 1)
 				{
-					pos.x += 1.0f;
+					pos.x += 10.0f;
 
 					if (pos.x > flail::FLAIL_RADIUS)
 					{
