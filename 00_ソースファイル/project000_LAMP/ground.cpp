eeaa29700c223b4df4194e2a1e3bc900c56b5afb
MorikawaShunya <221014@st.yoshida-g.ac.jp>
2023-12-14 15:38:45 +0900

    ステージ23とステージ用のテクスチャ追加

--- 00_ソースファイル/project000_LAMP/ground.cpp
+++ 00_ソースファイル/project000_LAMP/ground.cpp
@@ -39,6 +39,24 @@ const char *CGround::mc_apTextureFile[][6] =	// テクスチャ定数
 		"data\\TEXTURE\\block000.png",	// 前テクスチャ
 		"data\\TEXTURE\\block000.png",	// 後テクスチャ
 	},
+
+	{ // 氷テクスチャ
+		"data\\TEXTURE\\iceblock.png",	// 左テクスチャ
+		"data\\TEXTURE\\iceblock.png",	// 右テクスチャ
+		"data\\TEXTURE\\iceblock.png",	// 下テクスチャ
+		"data\\TEXTURE\\iceblock.png",	// 上テクスチャ
+		"data\\TEXTURE\\iceblock.png",	// 前テクスチャ
+		"data\\TEXTURE\\iceblock.png",	// 後テクスチャ
+	},
+
+	{ // 火山灰(黒)テクスチャ
+		"data\\TEXTURE\\ash.png",	// 左テクスチャ
+		"data\\TEXTURE\\ash.png",	// 右テクスチャ
+		"data\\TEXTURE\\ash.png",	// 下テクスチャ
+		"data\\TEXTURE\\ash.png",	// 上テクスチャ
+		"data\\TEXTURE\\ash.png",	// 前テクスチャ
+		"data\\TEXTURE\\ash.png",	// 後テクスチャ
+	},
 };
 
 //************************************************************
