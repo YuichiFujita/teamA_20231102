5f87f81ab40186409f4f4b1e9deebcc4f7afa18a
藤田勇一 <221065@st.yoshida-g.ac.jp>
2023-12-20 23:33:42 +0900

    nextPoint.cpp,hを追加：チュートリアルのスキップ管理 stage:nextPointの位置大きさ読み込み追加 object3D:GetMtx追加 object:ラベル追加 ready:priority低い問題解決

--- 00_ソースファイル/project000_LAMP/object.h
+++ 00_ソースファイル/project000_LAMP/object.h
@@ -70,6 +70,7 @@ public:
 		LABEL_UI,			// UI
 		LABEL_NUMBER,		// 数字
 		LABEL_PLAYER,		// プレイヤー
+		LABEL_NEXT,			// 遷移位置
 		LABEL_SPAWNPOINT,	// 生成位置
 		LABEL_ITEMPOINT,	// アイテム生成位置
 		LABEL_GROUND,		// 地盤
